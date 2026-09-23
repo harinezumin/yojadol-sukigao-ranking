9ba1d93878be01ebab20cc375152c8805f894234
harinezumin <objekt.new.4@gmail.com>
2026-09-24 01:41:43 +0900

    Add new members to the group Atheart

--- script.js
+++ script.js
@@ -301,6 +301,41 @@ const idols = [
   group: "UNCHILD",
   image: "IMG_0997.jpeg"
 },
+{
+  name: "Michi",
+  group: "Atheart",
+  image: "IMG_0998.jpeg"
+},
+{
+  name: "Arin",
+  group: "Atheart",
+  image: "IMG_0999.jpeg"
+},
+{
+  name: "Katelyn",
+  group: "Atheart",
+  image: "IMG_1000.jpeg"
+},
+{
+  name: "Bome",
+  group: "Atheart",
+  image: "IMG_1001.jpeg"
+},
+{
+  name: "Seohyeon",
+  group: "Atheart",
+  image: "IMG_1002.jpeg"
+},
+{
+  name: "Aurora",
+  group: "Atheart",
+  image: "IMG_1003.jpeg"
+},
+{
+  name: "Nahyun",
+  group: "Atheart",
+  image: "IMG_1004.jpeg"
+}
 
 ];
 
